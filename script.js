2d8ed5ae37a51cb98ee3cdf4957bd8a82f6a0131
rick <philipesferreiraa@gmail.com>
2022-07-23 12:36:32 -0300

    js ok

---
+++ script.js
@@ -0,0 +1,34 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function upadateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fizZero(hour)}:${fizZero(minute)}:${fizZero(second)}`;
+
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+}
+
+function fizZero(time) {
+    // if (time < 10) {
+    //     return '0' + time;
+    // } else {
+    //     return time;
+    // }
+
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(upadateClock, 1000);
+upadateClock();
